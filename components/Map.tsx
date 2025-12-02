13cd9157e84395ce9a16bfe875f1a1cdc9f2966c
Huseyin Kara <67840358+benhuseyin@users.noreply.github.com>
2025-12-02 18:50:31 +0300

    [REFACTOR] Fix mobile design of map section

--- components/Map.tsx
+++ components/Map.tsx
@@ -16,7 +16,7 @@ export default function Map() {
                 <div className="max-w-5xl mx-auto">
                     <div className="relative rounded-2xl overflow-hidden">
                         {/* Map iframe */}
-                        <div className="aspect-video w-full">
+                        <div className="h-[450px] md:h-auto md:aspect-video w-full">
                             <iframe
                                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3010.2976666666665!2d29.0!3d41.0!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDHCsDAwJzAwLjAiTiAyOcKwMDAnMDAuMCJF!5e0!3m2!1sen!2str!4v1234567890"
                                 width="100%"
@@ -31,7 +31,7 @@ export default function Map() {
                         </div>
 
                         {/* Overlay card */}
-                        <div className="absolute bottom-6 left-6 right-6 glass-strong rounded-xl p-6">
+                        <div className="absolute bottom-4 left-4 right-4 md:bottom-6 md:left-6 md:right-6 glass-strong rounded-xl p-4 md:p-6">
                             <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-4">
                                 <div className="flex items-start gap-3">
                                     <MapPin className="w-6 h-6 text-white flex-shrink-0 mt-1" />
